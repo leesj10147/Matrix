8ad1792becc8072c08e24b30988bc432a284ddcf
이 승재 <fkddl1436@naver.com>
2017-09-12 20:36:52 +0900

    add function 1) equals (Override) 2) winograd algorithm

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,3 +1,5 @@
+import org.omg.CORBA.MARSHAL;
+
 public final class Matrix {
     private final int[][] value;// value must be not change
     private final int row_size;
@@ -77,9 +79,13 @@ public final class Matrix {
     }
     public static Matrix strassen(Matrix a, Matrix b)
     {
-        if(a.row_size == 1)
-            return Matrix.multiply(a, b);
         int[][] result = new int[a.row_size][a.column_size];
+        if(a.row_size == 1)
+        {
+            result[0][0] = a.getValue(0, 0) * b.getValue(0, 0);
+            return new Matrix(result,0,result.length - 1,0, result[0].length - 1);
+        }
+
         int n = a.row_e;
         int nd2 = (a.row_e + a.row_s) / 2;
         int one = a.row_s;
@@ -131,5 +137,92 @@ public final class Matrix {
                         - P3.getValue(i - (nd2 + 1), j - (nd2 + 1)) - P7.getValue(i - (nd2 + 1), j - (nd2 + 1));
 
         return new Matrix(result,0,result.length - 1,0, result[0].length - 1);
+    }
+    @Override
+    public boolean equals(Object o)
+    {
+        Matrix m =(Matrix) o;
+        if(m.row_size != row_size || m.column_size != column_size)
+            return false;
+        for(int i = 0; i < m.row_size; ++i)
+            for(int j = 0; j < m.column_size; ++j)
+                if(getValue(i, j) != m.getValue(i, j))
+                    return false;
+        return true;
+    }
+
+    public static Matrix winograd(Matrix a, Matrix b)
+    {
+        int[][] result = new int[a.row_size][a.column_size];
+        if(a.row_size == 1)
+        {
+           result[0][0] = a.getValue(0, 0) * b.getValue(0, 0);
+            return new Matrix(result,0,result.length - 1,0, result[0].length - 1);
+        }
+
+        int n = a.row_e;
+        int nd2 = (a.row_e + a.row_s) / 2;
+        int one = a.row_s;
+
+        Matrix A11 = new Matrix(a.value, one, nd2, one, nd2);
+        Matrix A12 = new Matrix(a.value, one, nd2, nd2 + 1, n);
+        Matrix A21 = new Matrix(a.value, nd2 + 1, n, one, nd2);
+        Matrix A22 = new Matrix(a.value, nd2 + 1, n, nd2 + 1,n);
+
+        n = b.row_e;
+        nd2 = (b.row_e + b.row_s) / 2;
+        one = b.row_s;
+
+        Matrix B11 = new Matrix(b.value, one, nd2, one, nd2);
+        Matrix B12 = new Matrix(b.value, one, nd2, nd2 + 1, n);
+        Matrix B21 = new Matrix(b.value, nd2 + 1, n, one, nd2);
+        Matrix B22 = new Matrix(b.value, nd2 + 1, n, nd2 + 1,n);
+
+        Matrix S1 = Matrix.add(A21, A22);
+        Matrix S2 = Matrix.subtract(S1, A11);
+        Matrix S3 = Matrix.subtract(A11, A21);
+        Matrix S4 = Matrix.subtract(A12, S2);
+        Matrix S5 = Matrix.subtract(B12, B11);
+        Matrix S6 = Matrix.subtract(B22, S5);
+        Matrix S7 = Matrix.subtract(B22, B12);
+        Matrix S8 = Matrix.subtract(S6, B21);
+
+        Matrix P1 = Matrix.multiply(S2, S6);
+        Matrix P2 = Matrix.multiply(A11, B11);
+        Matrix P3 = Matrix.multiply(A12, B21);
+        Matrix P4 = Matrix.multiply(S3, S7);
+        Matrix P5 = Matrix.multiply(S1, S5);
+        Matrix P6 = Matrix.multiply(S4, B22);
+        Matrix P7 = Matrix.multiply(A22, S8);
+
+        Matrix T1 = Matrix.add(P1, P2);
+        Matrix T2 = Matrix.add(T1, P4);
+
+
+        one = 0;
+        nd2 = (result.length - 1) / 2;
+        n = result.length - 1;
+        for(int i = one; i <= nd2; ++i)
+            for (int j = one; j <= nd2; ++j)
+                result[i][j] = P2.getValue(i - one, j - one) + P3.getValue(i - one, j - one);
+
+        for(int i = one; i <= nd2; ++i)
+            for (int j = nd2 + 1; j <=n; ++j)
+                result[i][j] = T1.getValue(i - one, j - nd2 - 1) + P5.getValue(i - one, j - nd2 - 1)
+                        + P6.getValue(i - one, j - nd2 - 1);
+
+
+        for(int i = nd2 + 1; i <= n; ++i)
+            for(int j = one; j <= nd2; ++j)
+                result[i][j] = T2.getValue(i - nd2 - 1, j - one) - P7.getValue(i - nd2 - 1, j - one);
+
+
+        for(int i = nd2 + 1; i <= n; ++i)
+            for(int j = nd2 + 1; j <= n; ++j)
+                result[i][j] = T2.getValue(i - nd2 - 1, j - nd2 - 1) + P5.getValue(i - nd2 - 1, j - nd2 - 1);
+
+        return new Matrix(result,0,result.length - 1,0, result[0].length - 1);
+
+
     }
 }
